82043102e97c13edaeca36288322949133997191
Subhadeep Sen <subhadeep0977@gmail.com>
2021-03-01 22:02:53 +0530

    renamed app starter

--- src/main/java/dr/sens/dental/clinic/app/ClinicManagementApplicationStarter.java
+++ src/main/java/dr/sens/dental/clinic/app/ClinicManagementApplicationStarter.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "dr.sens.dental.clinic.*")
-public class DrSensDentalClinicApplication {
+public class ClinicManagementApplicationStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DrSensDentalClinicApplication.class, args);
+		SpringApplication.run(ClinicManagementApplicationStarter.class, args);
 	}
 }
